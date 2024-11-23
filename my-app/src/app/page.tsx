e48a1331c00d7529bc451fde979551a78b05b797
Manoel <m.moreirazika145@gmail.com>
2024-11-23 14:49:13 -0300

    feat: first commit

---
+++ my-app/src/app/page.tsx
@@ -0,0 +1,11 @@
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <div className={styles.page}>
+      <h1>
+        Óla Mundo!
+      </h1>
+    </div>
+  );
+}
